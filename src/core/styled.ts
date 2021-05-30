cd0b4549aa7cf7bb5d92d9724cec20bfaaeea4e3
maxim760 <maks.tomanov@mail.ru>
2021-05-31 00:39:02 +0300

    delete unused

--- src/core/styled.ts
+++
@@ -1,6 +0,0 @@
-export function withProps<A, B>(component: A, properties: B): A & B {
-  Object.keys(properties).forEach((key) => {
-    (component as any)[key] = (properties as any)[key];
-  });
-  return component as A & B;
-}
